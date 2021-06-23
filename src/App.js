04e8a240c0f4c4a66bcd4dde708831ad770f9c18
malzak <malzovich@gmail.com>
2021-06-23 09:48:56 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,26 +5,6 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       vegeta: 100,
-       goku: 100
-    }
-  }
-
-  reduceThe = (param, param_) => {
-    if(param === 'Goku'){
-       this.setState({
-         vegeta: this.state.vegeta - param_
-       })
-    }else{
-      this.setState({
-        goku: this.state.goku - param_
-      })
-    }
-  }
   
   render(){
     
